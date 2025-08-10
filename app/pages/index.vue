7782cee3e91cee8c0073bf922368108dafc2a462
TouHikari <tang20051026@foxmail.com>
2025-08-10 19:03:20 +0800

    fix: 修复启动问题

---
+++ app/pages/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const { data: home } = await useAsyncData(() => queryCollection('content').path('/index').first())
+
+useSeoMeta({
+  title: home.value?.title || 'TouHikari.top',
+  description: home.value?.description || '欢迎来到 TouHikari 的个人数字空间'
+})
+</script>
+
+<template>
+  <ContentRenderer v-if="home" :value="home" />
+  <div v-else>Home not found</div>
+</template>
