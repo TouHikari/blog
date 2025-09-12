44199d2ae45cd1119413657b873a38efa04c7507
TouHikari <tang20051026@foxmail.com>
2025-09-12 20:34:52 +0800

    feat(layout, title): 完善首页布局，增加标题打字效果

---
+++ app/components/HomeLinks.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="links-container">
+    <h4>链接</h4>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+.links-container {
+  margin: 1rem auto;
+}
+</style>
