3d54c494990b7c2cc97c4fdc9eacc31b00e97cf0
TouHikari <tang20051026@foxmail.com>
2025-09-12 21:48:30 +0800

    feat(title, animation): 增加主页标题启动闪烁动画

--- app/components/HomeTitle.vue
+++ app/components/HomeTitle.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="title-container">
-    <h1 class="title">Where shadows dance with data streams.<span class="blink-fast"> |</span></h1>
+    <h1 class="title bootup">Where shadows dance with data streams.<span class="blink-fast"> |</span></h1>
     <hr />
     <div class="slogan-container">
       <img></img>
@@ -24,6 +24,10 @@
   animation: blink 1s infinite;
 }
 
+.bootup {
+  animation: bootup 2s ease;
+}
+
 @keyframes blink {
   0%,
   50% {
@@ -32,6 +36,71 @@
   51%,
   100% {
     opacity: 0;
+  }
+}
+
+@keyframes bootup {
+  0%,
+  10% {
+    opacity: 0;
+  }
+  11%,
+  30% {
+    opacity: 0.1;
+  }
+  31%,
+  35% {
+    opacity: 0.6;
+  }
+  36%,
+  45% {
+    opacity: 0.1;
+  }
+  46%,
+  50% {
+    opacity: 0.2;
+  }
+  51%,
+  60% {
+    opacity: 0;
+  }
+  61%,
+  80% {
+    opacity: 0.2;
+  }
+  81%,
+  90% {
+    opacity: 0.9;
+  }
+  91% {
+    opacity: 0.91;
+  }
+  92% {
+    opacity: 0.92;
+  }
+  93% {
+    opacity: 0.93;
+  }
+  94% {
+    opacity: 0.94;
+  }
+  95% {
+    opacity: 0.95;
+  }
+  96% {
+    opacity: 0.96;
+  }
+  97% {
+    opacity: 0.97;
+  }
+  98% {
+    opacity: 0.98;
+  }
+  99% {
+    opacity: 0.99;
+  }
+  100% {
+    opacity: 1;
   }
 }
 
