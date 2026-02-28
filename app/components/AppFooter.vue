2411596e96834ce02aa3ac2dc401b3d070342163
TouHikari <tang20051026@foxmail.com>
2026-02-28 20:50:13 +0800

    style: 移除未使用的空媒体查询占位符

--- app/components/AppFooter.vue
+++ app/components/AppFooter.vue
@@ -255,20 +255,4 @@ onUnmounted(() => {
     text-align: center;
   }
 }
-
-@media (min-width: #{$breakpoint-mobile}) and (max-width: #{$breakpoint-tablet - 1px}) {
-  //
-}
-
-@media (min-width: #{$breakpoint-tablet}) and (max-width: #{$breakpoint-desktop - 1px}) {
-  //
-}
-
-@media (min-width: #{$breakpoint-desktop}) and (max-width: #{$breakpoint-desktop-lg - 1px}) {
-  //
-}
-
-@media (min-width: #{$breakpoint-desktop-lg}) {
-  //
-}
 </style>
